de8639e0d8384d9efec58f5642e292a9dd97ef7b
Kay van Bree <kayvanbree@gmail.com>
2019-09-22 12:05:33 +0200

    Add Springboot stuff

---
+++ src/main/java/com/kayvanbree/rockstardb/RockstardbApplication.java
@@ -0,0 +1,13 @@
+package com.kayvanbree.rockstardb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockstardbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockstardbApplication.class, args);
+	}
+
+}
